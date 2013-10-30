e683a9a7cb7d1f43f5881292a2c7e2930771562b
Mark Haskamp <mark.haskamp@pinnsg.com>
2013-10-30 16:46:02 -0400

    filling in rows of the table

---
+++ ex4_9/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ex4_9
+//
+//  Created by Mark Haskamp on 10/30/13.
+//  Copyright (c) 2013 Mark Haskamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+@end
--- ex4_9/tv49AppDelegate.m
+++ ex4_9/tv49AppDelegate.m
@@ -7,13 +7,17 @@
 //
 
 #import "tv49AppDelegate.h"
+#import "ViewController.h"
 
 @implementation tv49AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+
+    ViewController *controller = [[ViewController alloc] initWithStyle:UITableViewStylePlain];
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    self.window.rootViewController = controller;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
